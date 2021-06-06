44e18c436039e989f3daf3e65965fd6684c2a62f
chris <cseiter@gmail.com>
2021-06-06 10:21:56 -0400

    create initial fetch in QuestionList omponent

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,6 +1,15 @@
-import React from "react";
+import React,{useEffect,useState} from "react";
 
 function QuestionList() {
+  const [getQuestions,setQuestions] = useState(null);
+  useEffect (() => {
+    fetch("http://localhost:4000/questions")
+    .then((r) => r.json())
+    .then((getQuestions) => {
+      setQuestions(getQuestions);
+    });
+  },[]);
+
   return (
     <section>
       <h1>Quiz Questions</h1>
